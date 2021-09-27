f4b4bb6236b5ce1e493eed41d2886c27707495f3
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-09-27 16:51:19 +0100

    simple react workshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Welcome = React.lazy(() => import("./views/Welcome"));
+const Products = React.lazy(() => import("./views/Products"));
+const ProductDetail = React.lazy(() => import("./views/ProductDetail"));
 
 function App() {
+
+  const connectedUser = undefined;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <AppFrame className="App">
+        <BrowserRouter basename="/">
+          <Suspense fallback={<p>...Loading page please wait</p>}>
+        {connectedUser?(
+          <Switch>
+          <Route
+          path="/welcome"
+          render={(props) => <Welcome {...props} />}
+          ></Route>
+          <Route
+          path="/products"
+          render={(props) => <Products {...props} />}
+          ></Route>
+          <Route
+          path="/product/:name"
+          render={(props) => <ProductDetail {...props} />}
+          ></Route>
+          <Route exact render={() => (<p>default rendered page!Welcome {connectedUser.name}</p>)}></Route>
+          </Switch>
+          ) : (
+          <Switch>
+          <Route
+          path="/welcome"
+          render={(props) => <Welcome {...props} />}>
+          </Route>
+          <Route
+          path="/login"
+          render={(props) => <Welcome {...props} />}>
+          </Route>
+          <Route
+          path="/products"
+          render={(props) => <Products {...props} />}>
+          </Route>
+          <Route
+          path="/product/:name"
+          render={(props) => <ProductDetail {...props} />}>
+
+          </Route>
+          <Route exact render={() => <p>You are not logged in!</p>}>
+
+          </Route>
+          </Switch>
+        )}
+          </Suspense>
+        </BrowserRouter>
+      </AppFrame>
+    </>
   );
 }
-
+const AppFrame = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
 export default App;
---
+++ src/views/ProductDetail.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import products from "../products.json";
+import Product from "../components/Product";
+export default class ProductDetails extends Component {
+  render() {
+    const name = this.props.match.params.name;
+    const toRender = products.filter((product) => product.name === name)[0];
+    console.log(this.props)
+    return (
+    <>
+        <button onClick={() => this.props.history.replace("/products")}>Return to products</button>
+      <ProductsWrapper>
+        {toRender ? (<Product prod={toRender}></Product>) : (<p>Product not found</p>)}
+      </ProductsWrapper>
+      </>
+    );
+  }
+}
+const ProductsWrapper = styled.div`
+  text-align: center;
+  display: flex;
+`;
